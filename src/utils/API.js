b0fd6c19db8831385dd8b2f74c7203d429f2c62b
Jon Free <freejonm@gmail.com>
2021-04-22 10:25:08 -0400

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASEURL = 'https://randomuser.me/api/?results=20&nat=us';
-
-export default {
-  employees: function (query) {
-    return axios.get(BASEURL + query);
-  }
-};
